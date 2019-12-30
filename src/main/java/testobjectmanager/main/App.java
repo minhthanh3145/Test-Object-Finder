96482903b38f56bd4a87bc79cbc0f899633e9eeb
bachld <bachld@vng.com.vn>
2019-12-30 10:59:17 +0700

    Add naive approach to searching for query

--- src/main/java/testobjectmanager/main/App.java
+++ src/main/java/testobjectmanager/main/App.java
@@ -1,9 +1,86 @@
 package testobjectmanager.main;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+import java.lang.System;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        
+        String pathToObjectFolder;
+        if (args.length <= 1) {
+            pathToObjectFolder = "./Object Repository";
+        } else {
+            pathToObjectFolder = args[1] + "./Object Repository";
+        }
+
+        String queryTag = "randomTag";
+        String queryValue = "randomValue";
+
+        long beginTime = System.nanoTime();
+        List<String> results = searchForTagWithValueInFolder(queryTag, queryValue, pathToObjectFolder);
+        long endTime = System.nanoTime();
+        System.out.println("Search elapsed: " + (endTime - beginTime) + "with " + results.size() + " results.");
+    }
+
+
+    private static ArrayList<String> searchForTagWithValueInFolder(
+            String queryTag, String queryValue, String pathToObjectFolder) throws IOException {
+
+        ArrayList<File> allRsFiles = getAllRsFilesInFolder(pathToObjectFolder, true);
+        ArrayList<String> results = new ArrayList<String>();
+
+        for (File rsFile : allRsFiles) {
+            if (xmlContainsQueryWithValue(rsFile, queryTag, queryValue)) {
+                results.add(rsFile.getCanonicalPath());
+            }
+        }
+
+        return results;
+    }
+
+    private static ArrayList<File> getAllRsFilesInFolder(String folder, boolean recursive) throws IOException {
+        File folderFile = new File(folder);
+        ArrayList<File> files = new ArrayList<File>();
+
+        for (File file : folderFile.listFiles()) {
+            if (file.isFile()) {
+                if (file.getName().toLowerCase().endsWith(".rs")) {
+                    files.add(file);
+                }
+            } else if (recursive) {
+                files.addAll(getAllRsFilesInFolder(file.getCanonicalPath(), recursive));
+            }
+        }
+
+        return files;
+    }
+
+    private static boolean xmlContainsQueryWithValue(File file, String tag, String value) {
+        try {
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(file);
+			doc.getDocumentElement().normalize();
+
+			NodeList nodes = doc.getElementsByTagName(tag);
+
+			for (int nodeIndex = 0; nodeIndex < nodes.getLength(); ++nodeIndex) {
+				Element element = (Element) nodes.item(nodeIndex);
+				if (element.getTextContent().contains(value)) {
+					return true;
+				}
+			}
+			return false;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
